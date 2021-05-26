1b7624d554a20ec6cdaa6b6e781659338f96fc78
Matej-Omazic <omazzza@gmail.com>
2021-05-26 12:35:23 +0200

    Services prilagoden za backend (dohvat jednog dokumenta)

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 let Service = axios.create({
     baseURL: 'http://localhost:3000',
-    timeout: 1000
+    timeout: 3000
 })
 
 let Games = {
@@ -10,12 +10,26 @@ let Games = {
         return Service.game('/games',game)
     },
 
+    async getOne(id){
+        let response = await Service.get(`/games/${id}`)
+        let element = response.data
+        return{
+            id: element._id,
+            url: element.img_url,
+            game_name: element.name,
+            genre: element.genre,
+            rel_date: element.release_date,
+            rank: element.rate,
+            route: element.route
+          };
+    },
+
     async getAll(search_text){
         let options = {};
 
         if (search_text) {
             options.params = {
-                name: search_text
+                name2: search_text
             };
         }
         let response = await Service.get('/games',options)
